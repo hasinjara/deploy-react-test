4b4e2caa6d0b0ebdab3ff735835f7a074364d213
Hasinjara <wenzelhasinjara@gmail.com>
2023-12-26 13:15:00 +0300

    update

---
+++ src/components/button/button.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { ButtonWrapper } from './button.styled';
+
+interface ButtonProps {}
+
+const Button: FC<ButtonProps> = () => (
+ <ButtonWrapper>
+    Button Component
+ </ButtonWrapper>
+);
+
+export default Button;
